236ecfe3e930051cf3d213d1e418035ecc6d273f
maghlifiissam-art <maghlifiissam@gmail.com>
2026-09-16 06:36:17 +0100

    Change model from llama3 to openai/gpt-oss-120b

--- app.py
+++ app.py
@@ -33,8 +33,7 @@ with tab1:
             with st.spinner("Processing generation via Groq..."):
                 try:
                     completion = client.chat.completions.create(
-                        model="llama3-70b-8192",
-
+                        model="openai/gpt-oss-120b",
                         messages=[
                             {
                                 "role": "system",
